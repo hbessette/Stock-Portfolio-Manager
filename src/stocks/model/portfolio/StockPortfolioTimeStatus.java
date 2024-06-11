665262e41fc4703b8dadd5d1485fa847197125c6
Max Selby <maxselby64@gmail.com>
2024-06-11 14:47:46 -0400

    e

---
+++ src/stocks/model/portfolio/StockPortfolioTimeStatus.java
@@ -0,0 +1,16 @@
+package stocks.model.portfolio;
+
+import java.util.Date;
+
+import stocks.model.portfolio.shares.StockAndShares;
+import stocks.model.stock.Stocks;
+
+public class StockPortfolioTimeStatus {
+  StockAndShares[] stocksAndShares;
+
+  public StockPortfolioTimeStatus
+
+  public StockAndShares[] getStocksAndShares() {
+    return stocksAndShares;
+  }
+}
